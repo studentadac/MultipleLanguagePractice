6580c8205ddc720a63ba3239f74c404d1ac4a756
Sawae Yota <quarz.y.s@gmail.com>
2017-12-09 02:19:30 +0900

    再帰の追加と関数化

--- Chap4c/P4-4/Clang.c
+++ Chap4c/P4-4/Clang.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
+int reverse(int x) {
+  if (x <= 1)
+    return (1);
+  return x + reverse(x - 1); //再帰的に求める
+}
+
+int sum(int x) {
+  return x * (x + 1) / 2; //三角数で求める
+}
+int loop(int x) //単純に加算して求める
+{
+  int counter = 0;
+  for (int i = 1; i <= x; i++) {
+    counter += i;
+  }
+  return counter;
+}
+
 int main() {
   int upr;
   printf("数値を入力してください\n");
   scanf("%d", &upr);
-  int answer = upr * (upr + 1) / 2; //三角数でも止める
-  printf("%d\n", answer);
-  int counter = 0;
-  for (int i = 1; i <= upr; i++) {
-    counter += i;
-  }
-  printf("%d\n", counter); //単純に加算して求める
-
+  printf("再帰%d\t\n", reverse(upr));
+  printf("加算%d\t\n", loop(upr));
+  printf("三角数%d\t\n", sum(upr));
   return 0;
 }

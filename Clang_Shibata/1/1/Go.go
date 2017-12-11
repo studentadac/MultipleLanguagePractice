f480f696e71f05aee1e34502d14d2db3fa63fd37
symulti <31360903+symulti@users.noreply.github.com>
2017-12-11 17:58:17 +0900

    1-1解答追加

---
+++ Clang_Shibata/1/1/Go.go
@@ -0,0 +1,7 @@
+package main
+import "fmt"
+func main(){
+    // Your code here!
+    
+    fmt.Println(15+37)
+}
---
+++ Clang_Shibata/1/1/cpp.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    // Your code here!
+    std::cout << 15 + 37 << std::endl;
+}
